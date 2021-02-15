8255d41b7558fad053ed0bc02ffbcd1774b42bc3
irchorn <70407458+irchorn@users.noreply.github.com>
2021-02-15 10:10:47 -0500

    Update assignment1_part2.py

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,10 +1,18 @@
 
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
 class Book:
-    def __init(self, author, title):
-        pass
-    
+    def __init__(self, title="", author="" ):
+        self.author=author
+        self.title=title
     def display(self):
-        pass
-        
-if __name__ == "__main__":
-    pass
+        print(self.title + ", written by " +  self.author)
+b1 = Book("Of Mice and Men", "John Steinbeck" )
+b1.display()
+b2 = Book("To Kill a Mockingbird", "Harper Lee")
+b2.display()
+
